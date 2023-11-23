e49ac2be1da14c2713d5219d4cf3c9e911849d5b
ptorija <pablotorija23@gmail.com>
2023-11-23 16:45:21 +0100

    project structure

---
+++ data/data.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/34685/OneDrive/Documentos/DataVisualizationProject")
+
+data <- read.csv("C:/Users/34685/OneDrive/Documentos/DataVisualizationProject/data/your_file.csv")
+
+head(data)
